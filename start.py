e0b023cbc697f7408daf9d0a8cbd0165651f14e1
TheRealH4ack3r <ep1007@icloud.com>
2021-04-22 09:37:35 -0400

    First In Class Commit

--- start.py
+++ start.py
@@ -1,35 +1,466 @@
-print("")
-print("Student 1:")
-print ("What is their first name? ")
-studentName1 = input ()
-print ("What is their first class? ")
-student1Class1 = input ()
-print ("What is their seat number? ")
-student1Class1SeatNum = int(input ())
-print (studentName1 + " sits in seat " + str(student1Class1SeatNum) + " during " + student1Class1 + " first period.")
-print("Is this student infected? (True/False) ")
-student1Isinfected = input()
+print ("Enter Class Name: ")
+className = input ()
+
+print ("How many students are in this class? ")
+totalStudents = int(input ()) 
+
+if totalStudents == 2:
+    print("")
+    print("Student 1:")
+    print ("What is their first name? ")
+    studentName1 = input ()
+    print ("What is their seat number? ")
+    student1SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student1Isinfected = input()
 
 
-print("")
-print("Student 2:")
-print ("What is their first name? ")
-studentName2 = input ()
-print ("What is their first class? ")
-student2Class1 = input ()
-print ("What is their seat number? ")
-student2Class1SeatNum = int(input ())
-print (studentName2 + " sits in seat " + str(student2Class1SeatNum) + " during " + student2Class1 + " first period.")
-print("Is this student infected? (True/False) ")
-student2Isinfected = input()
+    print("")
+    print("Student 2:")
+    print ("What is their first name? ")
+    studentName2 = input ()
+    print ("What is their seat number? ")
+    student2SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student2Isinfected = input()
 
-if student1Isinfected == "True":
-    if student1Class1 == student2Class1:
-        if student2Class1SeatNum == student1Class1SeatNum + 1 | student1Class1SeatNum - 1:
-            print ("")
-            print (studentName2 + " needs to quarentine due to exposure to " + studentName1 + ".")
-elif student2Isinfected == "True":
-    if student2Class1 == student1Class1:
-        if student2Class1SeatNum == student1Class1SeatNum + 1 | student1Class1SeatNum -1:
-            print ("")
-            print (studentName1 + " needs to quarentine due to exposure to " + studentName2 + ".")
+
+
+
+if totalStudents == 12:
+            
+    print("")
+
+    print("")
+    print("This Is Your " + className + " Classroom:")
+    print("")
+    print("1  2  3  4")
+    print("5  6  7  8")
+    print("9 10 11 12")
+    print("")
+
+    print("")
+    print("Student 1:")
+    print ("What is their first name? ")
+    studentName1 = input ()
+    print ("What is their seat number? ")
+    student1SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student1Isinfected = input()
+
+
+    print("")
+    print("Student 2:")
+    print ("What is their first name? ")
+    studentName2 = input ()
+    print ("What is their seat number? ")
+    student2SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student2Isinfected = input()
+
+
+    print("")
+    print("Student 3:")
+    print ("What is their first name? ")
+    studentName3 = input ()
+    print ("What is their seat number? ")
+    student3SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student3Isinfected = input()
+            
+
+    print("")
+    print("Student 4:")
+    print ("What is their first name? ")
+    studentName4 = input ()
+    print ("What is their seat number? ")
+    student4SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student4Isinfected = input()
+
+
+    print("")
+    print("Student 5:")
+    print ("What is their first name? ")
+    studentName5 = input ()
+    print ("What is their seat number? ")
+    student5SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student5Isinfected = input()
+
+
+    print("")
+    print("Student 6:")
+    print ("What is their first name? ")
+    studentName6 = input ()
+    print ("What is their seat number? ")
+    student6SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student6Isinfected = input()
+
+
+    print("")
+    print("Student 7:")
+    print ("What is their first name? ")
+    studentName7 = input ()
+    print ("What is their seat number? ")
+    student7SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student7Isinfected = input()
+
+
+    print("")
+    print("Student 8:")
+    print ("What is their first name? ")
+    studentName8 = input ()
+    print ("What is their seat number? ")
+    student8SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student8Isinfected = input()
+
+
+    print("")
+    print("Student 9:")
+    print ("What is their first name? ")
+    studentName9 = input ()
+    print ("What is their seat number? ")
+    student9SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student9Isinfected = input()
+
+
+    print("")
+    print("Student 10:")
+    print ("What is their first name? ")
+    studentName10 = input ()
+    print ("What is their seat number? ")
+    student10SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student10Isinfected = input()
+
+
+    print("")
+    print("Student 11:")
+    print ("What is their first name? ")
+    studentName11 = input ()
+    print ("What is their seat number? ")
+    student11SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student11Isinfected = input()
+
+
+    print("")
+    print("Student 12:")
+    print ("What is their first name? ")
+    studentName12 = input ()
+    print ("What is their seat number? ")
+    student12SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student12Isinfected = input()
+
+    print("")
+    print("This Is Your " + className + " Classroom:")
+    print("")
+    print(studentName1 + " " + studentName2 + " " + studentName3 + " " + studentName4)
+    print("")
+    print(studentName5 + " " + studentName6 + " " + studentName7 + " " + studentName8)
+    print("")
+    print(studentName9 + " " + studentName10 + " " + studentName11 + " " + studentName12)
+    print("")
+
+
+
+if totalStudents == 24:
+            
+    print("")
+
+    print("")
+    print("This Is Your " + className + " Classroom:")
+    print("")
+    print(" 1   2   3   4")
+    print("")
+    print(" 5   6   7   8")
+    print("")
+    print(" 9  10  11  12")
+    print("")
+    print("13  14  15  16")
+    print("")
+    print("17  18  19  20")
+    print("")
+    print("21  22  23  24")
+    print("")
+
+    print("")
+    print("Student 1:")
+    print ("What is their first name? ")
+    studentName1 = input ()
+    print ("What is their seat number? ")
+    student1SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student1Isinfected = input()
+
+
+    print("")
+    print("Student 2:")
+    print ("What is their first name? ")
+    studentName2 = input ()
+    print ("What is their seat number? ")
+    student2SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student2Isinfected = input()
+
+
+    print("")
+    print("Student 3:")
+    print ("What is their first name? ")
+    studentName3 = input ()
+    print ("What is their seat number? ")
+    student3SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student3Isinfected = input()
+            
+
+    print("")
+    print("Student 4:")
+    print ("What is their first name? ")
+    studentName4 = input ()
+    print ("What is their seat number? ")
+    student4SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student4Isinfected = input()
+
+
+    print("")
+    print("Student 5:")
+    print ("What is their first name? ")
+    studentName5 = input ()
+    print ("What is their seat number? ")
+    student5SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student5Isinfected = input()
+
+
+    print("")
+    print("Student 6:")
+    print ("What is their first name? ")
+    studentName6 = input ()
+    print ("What is their seat number? ")
+    student6SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student6Isinfected = input()
+
+
+    print("")
+    print("Student 7:")
+    print ("What is their first name? ")
+    studentName7 = input ()
+    print ("What is their seat number? ")
+    student7SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student7Isinfected = input()
+
+
+    print("")
+    print("Student 8:")
+    print ("What is their first name? ")
+    studentName8 = input ()
+    print ("What is their seat number? ")
+    student8SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student8Isinfected = input()
+
+
+    print("")
+    print("Student 9:")
+    print ("What is their first name? ")
+    studentName9 = input ()
+    print ("What is their seat number? ")
+    student9SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student9Isinfected = input()
+
+
+    print("")
+    print("Student 10:")
+    print ("What is their first name? ")
+    studentName10 = input ()
+    print ("What is their seat number? ")
+    student10SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student10Isinfected = input()
+
+
+    print("")
+    print("Student 11:")
+    print ("What is their first name? ")
+    studentName11 = input ()
+    print ("What is their seat number? ")
+    student11SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student11Isinfected = input()
+
+
+    print("")
+    print("Student 12:")
+    print ("What is their first name? ")
+    studentName12 = input ()
+    print ("What is their seat number? ")
+    student12SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student12Isinfected = input()
+
+
+    print("")
+    print("Student 13:")
+    print ("What is their first name? ")
+    studentName13 = input ()
+    print ("What is their seat number? ")
+    student13SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student13Isinfected = input()
+
+
+    print("")
+    print("Student 14:")
+    print ("What is their first name? ")
+    studentName14 = input ()
+    print ("What is their seat number? ")
+    student14SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student14Isinfected = input()
+
+
+    print("")
+    print("Student 15:")
+    print ("What is their first name? ")
+    studentName15 = input ()
+    print ("What is their seat number? ")
+    student15SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student15Isinfected = input()
+            
+
+    print("")
+    print("Student 16:")
+    print ("What is their first name? ")
+    studentName16 = input ()
+    print ("What is their seat number? ")
+    student16SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student16Isinfected = input()
+
+
+    print("")
+    print("Student 17:")
+    print ("What is their first name? ")
+    studentName17 = input ()
+    print ("What is their seat number? ")
+    student17SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student17Isinfected = input()
+
+
+    print("")
+    print("Student 18:")
+    print ("What is their first name? ")
+    studentName18 = input ()
+    print ("What is their seat number? ")
+    student18SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student18Isinfected = input()
+
+
+    print("")
+    print("Student 19:")
+    print ("What is their first name? ")
+    studentName19 = input ()
+    print ("What is their seat number? ")
+    student19SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student19Isinfected = input()
+
+
+    print("")
+    print("Student 20:")
+    print ("What is their first name? ")
+    studentName20 = input ()
+    print ("What is their seat number? ")
+    student20SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student20Isinfected = input()
+
+
+    print("")
+    print("Student 21:")
+    print ("What is their first name? ")
+    studentName21 = input ()
+    print ("What is their seat number? ")
+    student21SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student21Isinfected = input()
+
+
+    print("")
+    print("Student 22:")
+    print ("What is their first name? ")
+    studentName22 = input ()
+    print ("What is their seat number? ")
+    student22SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student22Isinfected = input()
+
+
+    print("")
+    print("Student 23:")
+    print ("What is their first name? ")
+    studentName23 = input ()
+    print ("What is their seat number? ")
+    student23SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student23Isinfected = input()
+
+
+    print("")
+    print("Student 24:")
+    print ("What is their first name? ")
+    studentName24 = input ()
+    print ("What is their seat number? ")
+    student24SeatNum = int(input ())
+    print("Is this student infected? (True/False) ")
+    student24Isinfected = input()
+    
+
+
+
+    print("")
+    print("This Is Your " + className + " Classroom:")
+    print("")
+    print(studentName1 + " " + studentName2 + " " + studentName3 + " " + studentName4)
+    print("")
+    print(studentName5 + " " + studentName6 + " " + studentName7 + " " + studentName8)
+    print("")
+    print(studentName9 + " " + studentName10 + " " + studentName11 + " " + studentName12)
+    print("")
+    print(studentName13 + " " + studentName14 + " " + studentName15 + " " + studentName16)
+    print("")
+    print(studentName17 + " " + studentName18 + " " + studentName19 + " " + studentName20)
+    print("")
+    print(studentName21 + " " + studentName22 + " " + studentName23 + " " + studentName24)
+    print("")
+
+
+
+
+
+
+
+# if student1Isinfected == "True":
+#     if student1Class1 == student2Class1:
+#         if student2Class1SeatNum == student1Class1SeatNum + 1 or student1Class1SeatNum - 1:
+#             print ("")
+#             print (studentName2 + " needs to quarentine due to exposure to " + studentName1 + ".")
+# elif student2Isinfected == "True":
+#     if student2Class1 == student1Class1:
+#         if student2Class1SeatNum == student1Class1SeatNum + 1 or student1Class1SeatNum -1:
+#             print ("")
+#             print (studentName1 + " needs to quarentine due to exposure to " + studentName2 + ".")
