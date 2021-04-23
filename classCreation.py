dfa066145f374b70bc9c6192787c2a2bd04f54f6
TheRealH4ack3r <ep1007@icloud.com>
2021-04-23 09:34:10 -0400

    Third class commit added some awesome stuff like a start file

--- classCreation.py
+++ classCreation.py
@@ -403,16 +403,3 @@ if 12 < totalStudents <= 24:
 
 
 
-
-
-
-# if student1Isinfected == "True":
-#     if student1Class1 == student2Class1:
-#         if student2Class1SeatNum == student1Class1SeatNum + 1 or student1Class1SeatNum - 1:
-#             print ("")
-#             print (studentName2 + " needs to quarentine due to exposure to " + studentName1 + ".")
-# elif student2Isinfected == "True":
-#     if student2Class1 == student1Class1:
-#         if student2Class1SeatNum == student1Class1SeatNum + 1 or student1Class1SeatNum -1:
-#             print ("")
-#             print (studentName1 + " needs to quarentine due to exposure to " + studentName2 + ".")
---
+++ start.py
@@ -0,0 +1,13 @@
+print("What would you like to do? ")
+print("")
+print("Create Class (1) | Scan Class (2)")
+action = int(input())
+print("")
+
+if action == 1:
+
+    import classCreation
+
+if action == 2:
+
+    import scanClass
